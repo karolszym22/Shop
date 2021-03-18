ed1b719eae740812161da27ae06ffaffb6664654
Karol Szymański <karol2134@op.pl>
2021-03-18 15:52:05 +0100

    electronicProducts.js

---
+++ js/ElectronicProducts.js
@@ -0,0 +1,66 @@
+ let laptops = [
+    {
+        name: "Lenovo IdeaPad Gaming",
+        description: "Lenovo IdeaPad Gaming 3 15ARH05 15,6 AMD Ryzen 5 4600H - 8GB RAM - 512GB Dysk - GTX1650 Grafika - Win10",
+        price: 3249,
+        discount: false,
+        graphicsCard: "nVidia® GeForce GTX 1650 + AMD Radeon™ Graphics",
+        operatingSystem: "Windows 10 Home Edition",
+        ram: "8gb",
+        memory: "512gb SSD",
+        processor: "AMD Ryzen™ 5 4600H 3,0 - 4,0 GHz",
+        screen: "15,6 cala, 1920 x 1080 pikseli 60 Hz",
+        quantity: 15,
+        sold: 5,
+
+
+        
+    },
+    {
+        name: "Lenovo IdeaPad Gaming 3",
+        description: "Lenovo IdeaPad Gaming 3 15ARH05 15,6 120Hz AMD Ryzen 5 4600H - 16GB RAM - 512GB Dysk - GTX1650Ti Grafika - Win10",
+        price: 3699,
+        discount: false,
+        graphicsCard: "nVidia® GeForce GTX 1650 Ti + AMD Radeon™ Graphics",
+        operatingSystem: "Windows 10 Home Edition",
+        ram: "16gb",
+        memory: "512gb SSD",
+        processor: "AMD Ryzen™ 5 4600H 3,0 - 4,0 GHz",
+        screen: " 15,6 cala, 1920 x 1080 pikseli 120 Hz",
+        quantity: 19,
+        sold: 1,
+        
+    },
+    {
+        name: "ASUS X509JA-EJ073T",
+        description: "ASUS X509JA-EJ073T 15,6 Intel® Core™ i3-1005G1 - 8GB RAM - 512GB Dysk - Win10",
+        price: 2599,
+        discount: false,
+        graphicsCard: " Intel® UHD Graphics",
+        operatingSystem: "Windows 10 Home Edition",
+        ram: "8gb",
+        memory: "512gb SSD",
+        processor: " Intel® Core™ i3 10gen 1005G1 1,2 - 3,4 GHz",
+        screen: "15,6 cala, 1920 x 1080 pikseli",
+        quantity: 16,
+        sold: 4,
+        
+    },
+    {
+        name: "Acer Nitro 5",
+        description: "Acer Nitro 5 15,6 Intel® Core™ i5-9300H - 8GB RAM - 512GB SSD Dysk - GTX1650 Grafika - Win10",
+        price: 3899,
+        discount: false,
+        graphicsCard: "nVidia® GeForce GTX1650 + Intel UHD Graphics 630",
+        operatingSystem: "Windows 10 Home Edition",
+        ram: "8gb",
+        memory: "512 GB SSD",
+        processor: "Intel® Core™ i5 9gen 9300H 2,4 - 4,1 GHz",
+        screen: "15,6 cala, 1920 x 1080 pikseli",
+        quantity: 14,
+        sold: 6,
+        
+    },
+
+]
+
