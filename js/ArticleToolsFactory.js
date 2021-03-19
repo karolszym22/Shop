0a85cdeb212eaaa2309ef6d4ebe82e397796bbf0
Karol Szymański <karol2134@op.pl>
2021-03-19 17:30:48 +0100

    new method = showArticle()

--- js/ArticleToolsFactory.js
+++ js/ArticleToolsFactory.js
@@ -1,10 +1,25 @@
 import devices from "./ElectronicProducts.js";
+import ArticleToolsUI from "./ArticleToolsUI.js";
 
  export class ArticleToolsFactory
  {
      constructor()
      {
          
+     }
+
+     showArticle()
+     {
+         switch(article)
+         {
+             case 'laptops':
+                  const laptops = new ArticleToolsUI(article);
+             case 'shavers':
+                  const shavers = new ArticleToolsUI(article);
+             case 'eletronicks':
+                  const laptops = new ArticleToolsUI(article);
+                  const shavers = new ArticleToolsUI(article);
+         }
      }
     
  }
