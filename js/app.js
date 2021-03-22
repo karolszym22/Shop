99b54a544b89acc008871b7234eb547ce7c21662
Karol Szymański <karol2134@op.pl>
2021-03-22 17:49:52 +0100

    import redirection class to app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 import devices from "./ElectronicProducts.js";
 import {ArticleToolsFactory} from './ArticleToolsFactory.js'
+import {Redirection} from "./RedirectionButtons.js"
 
+
+const buttons = new Redirection();
 
 const articleBttn = document.querySelectorAll('.open-article');
 
