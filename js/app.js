405ee9ac9d1d1377f4f90b82fbf1ed749e80b81c
Karol Szymański <karol2134@op.pl>
2021-03-22 13:41:21 +0100

    localStorage testing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 
 import {ArticleToolsUI} from "./ArticleToolsUI.js";
-
+import devices from "./ElectronicProducts.js";
 const create = new ArticleToolsUI();
 
+const {laptops,shavers} = devices;
 
+localStorage.setItem("sds",JSON.stringify(shavers));
+console.log("teraz!!!!");
 document.querySelectorAll(".show-product").
 forEach(bttn =>bttn.addEventListener('click', e =>
 {
