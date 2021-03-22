71741793d7385969c63d2bcf6e7aba606679c7ca
Karol Szymański <karol2134@op.pl>
2021-03-22 17:46:38 +0100

    sd

---
+++ js/RedirectionButtons.js
@@ -0,0 +1,13 @@
+export class Redirection
+{
+      constructor()
+      {
+        const articleBttn = document.querySelectorAll('.open-article');
+        articleBttn.forEach(bttn => bttn.addEventListener('click', ev =>
+        {
+            const state = ev.target.getAttribute('data-state');
+            sessionStorage.setItem("state",state );
+            
+        }))
+      }
+}
--- js/app.js
+++ js/app.js
@@ -7,9 +7,4 @@ const articleBttn = document.querySelectorAll('.open-article');
 
 
 
-    articleBttn.forEach(bttn => bttn.addEventListener('click', ev =>
-    {
-        const state = ev.target.getAttribute('data-state');
-        sessionStorage.setItem("state",state );
-        
-    }))
+  
