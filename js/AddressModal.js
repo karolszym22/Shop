0747e3c81c3641530168d149ff35db224474b460
Karol Szymański <karol2134@op.pl>
2021-04-01 20:40:54 +0200

    new AddressModal.js method:  checkEmail();

--- js/AddressModal.js
+++ js/AddressModal.js
@@ -2,23 +2,40 @@ export class AddressModal
 {
     constructor()
     {
+       this.saveMailChanges();
        this.closeAddressModal();
+       
     }
 
 
 
 
  
-     
+    checkEmail()
+    {
+        var email = document.getElementById("email").value;
+        console.log(email);
+        var re = /^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*@([a-zA-Z0-9_-]+)(\.[a-zA-Z0-9_-]+)*(\.[a-zA-Z]{2,4})$/i;
+        if(email.match(re) == null)
+            alert('Ten adres jest nieprawidłowy');
+        else
+            alert('Ten adres jest prawidłowy');
+    }
+
     closeAddressModal()
     {
-
         const closeBttn = document.getElementById("close-address-modal");
         closeBttn.addEventListener("click", this.hideAddressModal);
     }
     hideAddressModal()
-    {   const modal = document.querySelector('.address-modal');
-        modal.style.display = "none";
+    {   
+        document.querySelector('.address-modal').style.display = "none";
+        document.querySelector('.dark-body').style.display = "none";
+    }
+    saveMailChanges()
+    {
+       const saveBttn = document.querySelector(".save-information");
+       saveBttn.addEventListener("click", this.checkEmail);
     }
 }
 
