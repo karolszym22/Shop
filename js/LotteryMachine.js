6b0ed49657fd8821aa1c2dde04527e881570e19c
Karol Szymański <karol2134@op.pl>
2021-08-02 18:28:43 +0200

    add new file: LotteryMachine.js and add new class: LotteryMachine in it

---
+++ js/LotteryMachine.js
@@ -0,0 +1,15 @@
+
+export class AddressModal 
+{
+    constructor()
+    {
+     this.scoreCard1 = "";
+     this.scoreCard2 = "";
+     this.scoreCard3 = "";
+     this.coupons = ["10%", "5%", "2%"];
+    }
+    couponDraw = () =>
+    {
+     
+    }
+}
