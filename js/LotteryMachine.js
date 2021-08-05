e960a16aec779552a7fb898d6dbb065337b3f6d9
Karol Szymański <karol2134@op.pl>
2021-08-05 21:15:09 +0200

    finishing couponDraw and showOnMachine methods

--- js/LotteryMachine.js
+++ js/LotteryMachine.js
@@ -3,38 +3,43 @@ export class LotteryMachine
 {
     constructor()
     {
-     console.log("ZACZYNAMYyyyyy!");              
-     this.scoreCard1 = "?";
-     this.scoreCard2 = "?";
-     this.scoreCard3 = "?";
+         
+     this.scoreCards = ["?", "?", "?"];
      this.coupons = ["10%", "5%", "2%"];
-     this.couponDraw();
+     this.numberIntervals = 0;
+     this.interval =  setInterval(this.couponDraw, 100);
+     
+    
+     
     }
+
+
+
 
     couponDraw = () =>
     {
-        
-     for(let i=0; i<5; i++)
-     {
-       window.setInterval(this.scoreDraw, 300);
-     }
-
-    }
-    scoreDraw = () =>
+     
+    
+    this.interval = true;
+    for (const i in this.scoreCards) 
     {
-       this.scoreCard1 =  this.coupons[Math.floor(Math.random()*this.coupons.length)];
-       this.scoreCard2 =  this.coupons[Math.floor(Math.random()*this.coupons.length)];
-       this.scoreCard3 =  this.coupons[Math.floor(Math.random()*this.coupons.length)];
-
-       this.drawResult(this.scoreCard1,this.scoreCard2,this.scoreCard3);
+       this.scoreCards[i] = this.coupons[Math.floor(Math.random()*this.coupons.length)] 
+       this.showOnMachine(this.scoreCards[i],i);
     }
-    drawResult = (draws) =>
+    this.numberIntervals++
+    if(this.numberIntervals===15)
     {
-       let [score1,score2,score3] = draws;
-       console.log(score1)
-       console.log(score2)
-       console.log(score3)
+      clearInterval(this.interval);
     }
+    }
+   
+    showOnMachine = (score,id) =>
+    {
+     document.getElementById(`machine-card-${parseInt(id)+1}`).innerHTML = score;
+    }
+    
+    
+    
     
     
 
