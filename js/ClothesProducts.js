c3f1e1ed16799eca34f63071cf79877de1378dfa
Karol Szymański <karol2134@op.pl>
2021-12-13 22:30:47 +0100

    new file: ClothesProducts.js

---
+++ js/ClothesProducts.js
@@ -0,0 +1,36 @@
+let clothes = [
+    {
+
+        producer: "H&M",
+        description: "Klasyczny męski T-shirt wykonany w 100% z wysokogatunkowej czesanej bawełny, która podnosi komfort użytkowania",
+        color: "czarny",
+        discount: false,
+        Composition: "100% Bawełna",
+        style: "Pasuje do każdego rodzaju stylizacji",
+        sold: 99,
+        price: 49.99,
+        productImage: "./images/czerwonaKoszula.jpg",
+        productImageZoom: "./images/asusZoom.jpg",
+        productId: "tshirt1",
+        name: "Black T-shirt",
+        
+    },
+    
+        {
+    
+            producer: "ZARA",
+            description: "Klasyczny męski sweter wykonany w 100% z wysokogatunkowej czesanej bawełny, która podnosi komfort użytkowania",
+            color: "zielona",
+            discount: false,
+            Composition: "100% Bawełna",
+            style: "Pasuje do każdego rodzaju stylizacji",
+            sold: 19,
+            price: 149.99,
+            productImage: "./images/zielonaKoszula.jpg",
+            productImageZoom: "./images/asusZoom.jpg",
+            productId: "tshirt2",
+            name: "Green T-shirt",
+            
+        },
+    
+]
