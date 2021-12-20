1fee97bda2b8042034152e30f6992f395c77cd74
Karol Szymański <karol2134@op.pl>
2021-12-20 12:50:27 +0100

    new pictures have been added and new array in ProductsInformation.js

--- js/ProductsInformation.js
+++ js/ProductsInformation.js
@@ -1,5 +1,5 @@
 let shavers = ["Producent:", "Opis:", "Typ zasilania:"];
 let laptops = ["Producent:", "Opis:", "Karta graficzna:", "Pamięć ram:", "Procesor:", "System operacyjny"]
+let tShirts = ["Producent:", "Opis:", "Kompozycja", "Kolor", "Styl"]
 
-
-export default {laptops,shavers};
+export default {laptops,shavers, tShirts};
