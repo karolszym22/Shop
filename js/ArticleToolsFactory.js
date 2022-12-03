a6479ab2c10f45053118f2b199accb365a2f301b
Karol Szymański <karol2134@op.pl>
2022-12-03 17:19:30 +0100

    Refactoring ArticleToolsFactory.js: - unassigning a value to a variable

--- js/ArticleToolsFactory.js
+++ js/ArticleToolsFactory.js
@@ -10,64 +10,64 @@ export class ArticleToolsFactory {
       case "laptops":
         document.querySelector(".category-name").innerHTML =
           "Elektronika i komputery";
-        const laptops = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "shavers":
         document.querySelector(".category-name").innerHTML =
           "Elektronika i komputery";
-        const shavers = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "tShirts":
         document.querySelector(".category-name").innerHTML =
           "Zwykłe ubrania oraz garnitury";
-        const tShirts = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "suits":
         document.querySelector(".category-name").innerHTML =
           "Zwykłe ubrania oraz garnitury";
-        const suits = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "motors":
         document.querySelector(".category-name").innerHTML =
           "Coś dla fanów dwóch kółek";
-        const motors = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "cars":
         document.querySelector(".category-name").innerHTML =
           "Coś dla fanów motoryzacji";
-        const cars = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "pot":
         document.querySelector(".category-name").innerHTML =
           "O rośliny trzeba dbać!";
-        const pot = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "chairs":
         document.querySelector(".category-name").innerHTML =
           "Przydałoby się tu umeblować coś";
-        const chairs = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "promotions":
         document.querySelector(".category-name").innerHTML =
           "Bierz póki tanio!";
-        const promotions = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
       case "foundItem":
         document.querySelector(".category-name").innerHTML = "Znalezione";
-        const foundItem = new ArticleToolsUI(article);
+        new ArticleToolsUI(article);
         sessionStorage.setItem("lastState", article);
         break;
     }
-  }
+  };
 }
 
 if (window.location.href == "http://127.0.0.1:5500/category.html") {
